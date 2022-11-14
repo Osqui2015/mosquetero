00f688f2d4b4e064e39b03c3090c36d8eb7eb95c
ESTEFANIAAVILA89 <105684764+ESTEFANIAAVILA89@users.noreply.github.com>
2022-11-13 21:07:00 -0300

    creacion de archivo

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Cards from './Cards'
+import Navbar from './Navbar'
+
 
 const Container = () => {
   return (
     <div className='container border'>
       <h1>aqui va el navbar</h1>
+      <Navbar/>
        <Cards />
     </div>
   )
