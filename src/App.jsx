8cdb1c4915eef1fa0c183f95812d602fb4a1f9ee
ESTEFANIAAVILA89 <105684764+ESTEFANIAAVILA89@users.noreply.github.com>
2022-11-17 20:01:55 -0300

    trabajo en el navbar y archivos contacto,destacado.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import Container from './components/Container'
-import Footer from './components/Footer'
-import HeaderNavb from './components/HeaderNavb'
+import React from 'react';
+import Container from './components/Container';
+import Footer from './components/Footer';
+import HeaderNavb from './components/HeaderNavb';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Login from './components/Login';
+import Destacado from './components/Destacado';
+import Contacto from './components/Contacto';
 
 
 const App = () => {
   return (
     <div>
       
+     
+     
+      
+      <BrowserRouter>
       <HeaderNavb/>
-        <Container />
+      <Routes>
+        <Route path='Login' element={<Login/>}/>
+        <Route path='Destacado' element={<Destacado/>}/>
+        <Route path='Contacto' element={<Contacto/>}/>
+      </Routes>
+      </BrowserRouter>
+      <Container />
       <Footer/>
     </div>
   )
