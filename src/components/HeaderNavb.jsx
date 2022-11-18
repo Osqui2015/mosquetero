a34560c188d899cd041c3c5d6a452c625084f654
ESTEFANIAAVILA89 <105684764+ESTEFANIAAVILA89@users.noreply.github.com>
2022-11-17 21:28:53 -0300

    colocacion de logo

--- src/components/HeaderNavb.jsx
+++ src/components/HeaderNavb.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import {Navbar,Container,Nav} from 'react-bootstrap';
- import {NavLink} from 'react-router-dom';
+import {NavLink, Link} from 'react-router-dom';
 
 function Header() {
     
   return (
-    <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">RSHOW</Navbar.Brand>
+   
+<Navbar bg="dark" variant="dark">
+    <Container>
+      <Link to="/">
+        <img
+          alt=""
+          src="/logo.png"
+          // width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+
+        </Link>
+          <Navbar.Brand to="/"> <img href="/img/logo.png" ></img> </Navbar.Brand>
           <Nav className="me-auto">
             <NavLink  className="nav-link" to="/">Home</NavLink>
             <NavLink className="nav-link" to="/Login">Login</NavLink>
