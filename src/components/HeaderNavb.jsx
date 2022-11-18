cd54637f1a48763d2d2461fea7b2f07a588ecd35
ESTEFANIAAVILA89 <105684764+ESTEFANIAAVILA89@users.noreply.github.com>
2022-11-17 22:45:32 -0300

    redes y trabajo en navbar

--- src/components/HeaderNavb.jsx
+++ src/components/HeaderNavb.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Navbar,Container,Nav} from 'react-bootstrap';
 import {NavLink, Link} from 'react-router-dom';
+import"../css/Archivo.css";
 
 function Header() {
     
@@ -16,23 +17,50 @@ function Header() {
           height="30"
           className="d-inline-block align-top"
         />{' '}
-
-        </Link>
-          <Navbar.Brand to="/"> <img href="/img/logo.png" ></img> </Navbar.Brand>
-          <Nav className="me-auto">
-            <NavLink  className="nav-link" to="/">Home</NavLink>
-            <NavLink className="nav-link" to="/Login">Login</NavLink>
-            <NavLink className="nav-link" to="/Contacto">Contacto</NavLink>
-            <NavLink className="nav-link" to="/Destacado">Destacado</NavLink>
-            <div class="container-fluid">
-            <form className='mt-4'>
-            <label> {" "} Buscar : <input type="text"  name='inputText'/> {" "} </label>
-            <button type='submit' className='btn btn-warning mx-2'><i class="bi bi-search"></i></button>
+      </Link>
+        
+      <Nav className="me-auto">
+        <NavLink  className="nav-link" to="/">Home</NavLink>
+        
+        <NavLink className="nav-link" to="/Contacto">Contacto</NavLink>
+        <NavLink className="nav-link" to="/Destacado">Destacado</NavLink>
+      </Nav>
+            
+      <div class="container-fluid searchBar d-flex align-content-center">
+        <form>
+          <label> {" "} Buscar : <input type="text"  name='inputText'/> {" "} </label>
+          <button type='submit' className='btn btn-warning'><i class="bi bi-search"></i></button>
         </form>
-            </div>
-            </Nav>
-        </Container>
-      </Navbar>
+      </div>
+
+      <div class="d-flex ">
+        <Link to="/">
+        <img
+          alt=""
+          src="/facebook.png"
+          // width="30"
+          height="30"
+   
+        />{' '}
+        </Link>
+        <Link to="https://www.facebook.com">
+        <img
+          alt=""
+          src="/instagram.png"
+          // width="30"
+          height="30"
+   
+        />{' '}
+        </Link>
+        </div>
+      
+      <Nav className="me-auto">
+      <NavLink className="nav-link" to="/Login">Login</NavLink>
+      </Nav>
+
+
+      </Container>
+    </Navbar>
   )
 }
 
