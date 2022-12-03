2de5bceaa8005d5c60033e6b3b6f89252c3b9ca3
ESTEFANIAAVILA89 <105684764+ESTEFANIAAVILA89@users.noreply.github.com>
2022-12-03 09:56:43 -0300

    comentarios de logica usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import { Route, Routes} from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-import Destacado from './pages/home/Destacado';
+import Destacado from "./pages/home/Destacado";
 
+import Nosotros from "./pages/home/Nosotros";
+import Terminos from "./pages/home/Terminos";
+import Contacto from "./pages/contact/ContactPage";
 
-import Nosotros from './pages/home/Nosotros';
-import Terminos from './pages/home/Terminos';
-import Contacto from './pages/contact/ContactPage';
-
-
-import './App.scss';
-import Layout from './components/Layout';
-import HomePage from './pages/home/HomePage';
-import ForgotPasswordPage from './pages/forgotPassword/ForgotPasswordPage';
-import BlogTwo from './pages/home/BlogTwo';
-
-
+import "./App.scss";
+import Layout from "./components/Layout";
+import HomePage from "./pages/home/HomePage";
+import ForgotPasswordPage from "./pages/forgotPassword/ForgotPasswordPage";
+import BlogTwo from "./pages/home/BlogTwo";
 
 const App = () => {
-  return ( 
-  <div>
-     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path='/destacado' element={<Destacado/>}/>
-        <Route path='/contacto' element={<Contacto/>}/>
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path='/Nosotros' element={ <Nosotros/> }/>
-        <Route path='/Terminos' element={<Terminos/>}/>
-        <Route path='/BlogTwo' element={<BlogTwo/>}/>
-      </Route>
-    </Routes>  
-    
-       {/*}  <BrowserRouter>
+  // aramar estructura de login(la logica y el estado que indica si esta logueado o no)
+  // 2. pasar por props a todas las partes que necesiten saber si esta logueado), home page, container,cards, card
+  //armar toda la estructura de los like, es lo mismo que el carrito.
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/destacado" element={<Destacado />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/Nosotros" element={<Nosotros />} />
+          <Route path="/Terminos" element={<Terminos />} />
+          <Route path="/BlogTwo" element={<BlogTwo />} />
+        </Route>
+      </Routes>
+
+      {/*}  <BrowserRouter>
           <HeaderNavb />
             <Routes>
               <Route path='Login' element={<Login/>}/>
@@ -46,8 +45,7 @@ const App = () => {
           <Footer />
         </BrowserRouter>{*/}
     </div>
+  );
+};
 
-)}
-
-
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,43 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Card = ({img}) => {
+const Card = ({ img }) => {
+  //la funcion handle tiene que revisar si esta logueado o no, si esta logueado va hacer un fetch a la api para agregar
+  //a favorito y sino tiene que hacer navigate a la pagina login paar qeu se loguee
+  const handleLike = () => {};
+
   return (
-         
-        <div className="card mb-3" >
-          <img src={ img } className="card-img-top" alt="imagen.png" />
-            <div className="card-body">
-              <h5 class="card-title">TITULO DEL SHOW</h5>    
-                <div className="container">
-                  <p className="card-text"> mini descripción </p>
-                </div>                                      
-            </div>
-              <div className="card-body">
-                <button type="button" className="btn mx-2 text-white" style={{ backgroundColor: "#FE6848" }}>Comprar</button>
-                <button type="button" className="btn btn-outline-danger"> <i className="bi bi-heart"></i> </button>
-              </div>
-          </div>
-  )
-}
+    <div className="card mb-3">
+      <img src={img} className="card-img-top" alt="imagen.png" />
+      <div className="card-body">
+        <h5 class="card-title">TITULO DEL SHOW</h5>
+        <div className="container">
+          <p className="card-text"> mini descripción </p>
+        </div>
+      </div>
+      <div className="card-body">
+        <button
+          type="button"
+          className="btn mx-2 text-white"
+          style={{ backgroundColor: "#FE6848" }}
+        >
+          Comprar
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-danger"
+          onClick={handleLike}
+        >
+          {" "}
+          <i className="bi bi-heart"></i>{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 Card.propTypes = {
-    img: PropTypes.string,
-}
+  img: PropTypes.string,
+};
 
-export default Card
+export default Card;
