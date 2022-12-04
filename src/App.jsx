58a78dfe293ae35657a1e6dc0843e3624d522c56
franconahuel20 <francoherrera.alu.frt@gmail.com>
2022-12-04 13:41:57 -0300

    Arreglo de App

--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,50 @@ import Nosotros from "./pages/home/Nosotros";
 import Terminos from "./pages/home/Terminos";
 import Contacto from "./pages/contact/ContactPage";
 
-import "./App.scss";
-import Layout from "./components/Layout";
-import HomePage from "./pages/home/HomePage";
-import ForgotPasswordPage from "./pages/forgotPassword/ForgotPasswordPage";
-import BlogTwo from "./pages/home/BlogTwo";
-import ShoppingCartPage from "./pages/shoppingCart/ShoppingCartPage";
-import Publicity from "./pages/home/Publicity";
-import BuyTicketPage from "./pages/buyTicket/BuyTicketPage";
+
+import './App.scss';
+import Layout from './components/Layout';
+import HomePage from './pages/home/HomePage';
+
+import ForgotPasswordPage from './pages/forgotPassword/ForgotPasswordPage';
+import BlogTwo from './pages/home/BlogTwo';
+import ShoppingCartPage from './pages/shoppingCart/ShoppingCartPage';
+import Publicity from './pages/home/Publicity';
+import BuyTicketPage from './pages/buyTicket/BuyTicketPage';
+import { PostProvider } from './context/postContext';
+import AddShows from './pages/home/AddShows';
+
+import {Toaster} from 'react-hot-toast'
+
 import Ticket from "./pages/shoppingCart/Ticket";
 import Tickets from "./pages/shoppingCart/Tickets";
 import Cart from "./pages/shoppingCart/Cart";
-import { PostProvider } from "./context/postContext";
-import AddShows from "./pages/home/AddShows";
-import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  return (
-    <div>
-      <PostProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/destacado" element={<Destacado />} />
-            <Route path="/contacto" element={<Contacto />} />
-            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-            <Route path="/Nosotros" element={<Nosotros />} />
-            <Route path="/Terminos" element={<Terminos />} />
-            <Route path="/BlogTwo" element={<BlogTwo />} />
-            <Route path="/ShoppingCart" element={<ShoppingCartPage />} />
-            <Route path="/BuyTicket" element={<BuyTicketPage />} />
-            <Route path="/Publicity" element={<Publicity />} />
-            <Route path="/Ticket" element={<Ticket />} />
-            <Route path="/Tickets" element={<Tickets />} />
-            <Route path="/Cart" element={<Cart />} />
-            <Route path="/AddShows" element={<AddShows />} />
-          </Route>
-        </Routes>
-        <Toaster />
-      </PostProvider>
-
-      {/*}  <BrowserRouter>
+  return ( 
+  <div>
+  <PostProvider>
+     <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/destacado' element={<Destacado/>}/>
+        <Route path='/contacto' element={<Contacto/>}/>
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path='/Nosotros' element={ <Nosotros/> }/>
+        <Route path='/Terminos' element={<Terminos/>}/>
+        <Route path='/BlogTwo' element={<BlogTwo/>}/>
+        <Route path='/ShoppingCart' element={<ShoppingCartPage/>}/>
+        <Route path='/BuyTicket' element={<BuyTicketPage/>}/>
+        <Route path='/Publicity' element={<Publicity/>}/>
+        <Route path='/AddShows' element={<AddShows/> }/>
+	      <Route path="/Ticket" element={<Ticket />} />
+        <Route path="/Tickets" element={<Tickets />} />
+        <Route path="/Cart" element={<Cart />} />        
+      </Route>
+    </Routes>
+    <Toaster/>  
+  </PostProvider>
+       {/*}  <BrowserRouter>
           <HeaderNavb />
             <Routes>
               <Route path='Login' element={<Login/>}/>
@@ -64,4 +67,5 @@ const App = () => {
   );
 };
 
-export default App;
+
+export default App
