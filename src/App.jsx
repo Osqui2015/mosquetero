4579fc78622bfe4bfd8b576de61e4bb238beb9c7
ESTEFANIAAVILA89 <105684764+ESTEFANIAAVILA89@users.noreply.github.com>
2022-12-04 22:20:58 -0300

    error 404

--- src/App.jsx
+++ src/App.jsx
@@ -7,53 +7,53 @@ import Nosotros from "./pages/home/Nosotros";
 import Terminos from "./pages/home/Terminos";
 import Contacto from "./pages/contact/ContactPage";
 
+import "./App.scss";
+import Layout from "./components/Layout";
+import HomePage from "./pages/home/HomePage";
 
-import './App.scss';
-import Layout from './components/Layout';
-import HomePage from './pages/home/HomePage';
+import ForgotPasswordPage from "./pages/forgotPassword/ForgotPasswordPage";
+import BlogTwo from "./pages/home/BlogTwo";
+import ShoppingCartPage from "./pages/shoppingCart/ShoppingCartPage";
+import Publicity from "./pages/home/Publicity";
+import BuyTicketPage from "./pages/buyTicket/BuyTicketPage";
+import { PostProvider } from "./context/postContext";
+import AddShows from "./pages/home/AddShows";
 
-import ForgotPasswordPage from './pages/forgotPassword/ForgotPasswordPage';
-import BlogTwo from './pages/home/BlogTwo';
-import ShoppingCartPage from './pages/shoppingCart/ShoppingCartPage';
-import Publicity from './pages/home/Publicity';
-import BuyTicketPage from './pages/buyTicket/BuyTicketPage';
-import { PostProvider } from './context/postContext';
-import AddShows from './pages/home/AddShows';
-
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from "react-hot-toast";
 
 import Ticket from "./pages/shoppingCart/Ticket";
 import Tickets from "./pages/shoppingCart/Tickets";
 import Cart from "./pages/shoppingCart/Cart";
+import error from "./pages/home/error";
 
 const App = () => {
-  return ( 
-  <div>
-  <PostProvider>
-     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path='/destacado' element={<Destacado/>}/>
-        <Route path='/contacto' element={<Contacto/>}/>
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path='/Nosotros' element={ <Nosotros/> }/>
-        <Route path='/Terminos' element={<Terminos/>}/>
-        <Route path='/BlogTwo' element={<BlogTwo/>}/>
-        <Route path='/ShoppingCart' element={<ShoppingCartPage/>}/>
-        <Route path='/BuyTicket' element={<BuyTicketPage/>}/>
-        <Route path='/Publicity' element={<Publicity/>}/>
-        <Route path='/AddShows' element={<AddShows/> }/>
+  return (
+    <div>
+      <PostProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/destacado" element={<Destacado />} />
+            <Route path="/contacto" element={<Contacto />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="/Nosotros" element={<Nosotros />} />
+            <Route path="/Terminos" element={<Terminos />} />
+            <Route path="/BlogTwo" element={<BlogTwo />} />
+            <Route path="/ShoppingCart" element={<ShoppingCartPage />} />
+            <Route path="/BuyTicket" element={<BuyTicketPage />} />
+            <Route path="/Publicity" element={<Publicity />} />
+            <Route path="/AddShows" element={<AddShows />} />
 
-        <Route path='/AddShows/:id' element={<AddShows/> }/>
+            <Route path="/AddShows/:id" element={<AddShows />} />
 
-	      <Route path="/Ticket" element={<Ticket />} />
-        <Route path="/Tickets" element={<Tickets />} />
-        <Route path="/Cart" element={<Cart />} />        
-      </Route>
-    </Routes>
-    <Toaster/>  
-  </PostProvider>
-       {/*}  <BrowserRouter>
+            <Route path="/Ticket" element={<Ticket />} />
+            <Route path="/Tickets" element={<Tickets />} />
+            <Route path="/Cart" element={<Cart />} />
+          </Route>
+        </Routes>
+        <Toaster />
+      </PostProvider>
+      {/*}  <BrowserRouter>
           <HeaderNavb />
             <Routes>
               <Route path='Login' element={<Login/>}/>
@@ -70,5 +70,4 @@ const App = () => {
   );
 };
 
-
-export default App
+export default App;
